0a8fc2778729f39bcede05a108ff06de073049fd
Israel Pires <pandamaroto964@gmail.com>
2025-07-13 17:14:51 -0300

    troca de imagem

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagm
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/foto_dia.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/foto_noite.png")
   }
 } 
