71077dcdf5ad4e3f939f5e8f6efeb6340dc27955
Israel Pires <pandamaroto964@gmail.com>
2025-07-14 14:47:39 -0300

    add certificate container

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// Mudança de cor
+
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  
+
   // Pegar a tag img
   const img = document.querySelector("#profile img")
-  
+
   // substituir a imagm
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
@@ -13,4 +15,29 @@ function toggleMode() {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/foto_noite.png")
   }
-} 
+}
+
+// mostra certificado
+
+function toggle(element) {
+  // adiciona/retira a classe "active" no switch clicado
+  element.classList.toggle("active")
+
+  // sobe até o pai .certificado e procura o .profile img
+  const parent = element.closest(".certificado")
+  const img = parent.querySelector(".profile img")
+
+  const display = window.getComputedStyle(img).display
+  img.style.display = display === "none" ? "block" : "none"
+}
+
+
+const switch2 = document.getElementById("switch_2")
+switch2.addEventListener("click", () => {
+  switch2.classList.toggle("active")
+})
+
+const switch3 = document.getElementById("switch_3")
+switch3.addEventListener("click", () => {
+  switch3.classList.toggle("active")
+})
